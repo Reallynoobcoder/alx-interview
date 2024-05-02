a52cc8f8f50c9e610493687f31fc875fd7e85cfe
El imali hamza <elimal.hamza@gmail.com>
2024-05-02 18:05:03 +0000

    .

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,12 +2,16 @@
 """
 script that reads stdin line by line and computes metrics.
 """
-
 from sys import stdin
+import random
+from time import sleep
 
 line_count = 0
 file_size = 0
-status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+status_codes = {
+    200: 0, 301: 0, 400: 0, 401: 0,
+    403: 0, 404: 0, 405: 0, 500: 0
+}
 
 try:
     for line in stdin:
@@ -20,15 +24,12 @@ try:
             status_codes[status_code] += 1
 
         if line_count % 10 == 0:
-            print(f"Total file size: File size: {file_size}")
+            print(f"File size: {file_size}")
             for code, count in sorted(status_codes.items()):
-                if count > 0:
-                    print(f"{code}: {count}")
+                print(f"{code}: {count}")
             print()
 
 except KeyboardInterrupt:
-    print("\nProgram interrupted. Printing current metrics:")
-    print(f"Total file size: File size: {file_size}")
+    print(f"File size: {file_size}")
     for code, count in sorted(status_codes.items()):
-        if count > 0:
-            print(f"{code}: {count}")
+        print(f"{code}: {count}")
