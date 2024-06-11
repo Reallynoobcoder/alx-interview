be399a94f84d6aa6838bbac08c65d0efb3c1e7ca
El imali hamza <elimal.hamza@gmail.com>
2024-06-11 18:11:52 +0000

    .

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
+"""0x09. Island Perimeter."""
+
 
 def island_perimeter(grid):
+    """
+    function def island_perimeter(grid): that returns the perimeter.
+    """
     perimeter = 0
-    
+
     rows = len(grid)
     cols = len(grid[0])
-    
+
     for r in range(rows):
         for c in range(cols):
             if grid[r][c] == 1:
@@ -17,5 +22,5 @@ def island_perimeter(grid):
                     perimeter += 1
                 if c == cols - 1 or grid[r][c + 1] == 0:
                     perimeter += 1
-    
+
     return perimeter
